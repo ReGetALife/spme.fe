8fb0e392a49cbfc3a2268801ca2e6122f440a287
GetALife <1340482017@qq.com>
2020-02-21 00:23:23 +0800

    update script

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker -v
 docker stop nginx_spme || true
 docker rm nginx_spme  || true
-docker run -d -p 20201:80 -v ./dist:/usr/share/nginx/html --name nginx_spme nginx
+docker run -d -p 20201:80 -v $PWD/dist:/usr/share/nginx/html --name nginx_spme nginx
