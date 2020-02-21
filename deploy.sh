a7ea603ebee7a963849397fc39af63d3d99dd48a
GetALife <1340482017@qq.com>
2020-02-22 00:11:46 +0800

    update ngnix config

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ docker rm nginx_spme  || true
 
 # make sure to export WORKSPACE, e.g.
 # export WORKSPACE=/home/user/spme/jenkins_node/workspace/spme_fe
-echo 'docker run -d -p 20201:80 -v $WORKSPACE/dist:/usr/share/nginx/html -v $WORKSPACE/nginx.conf:/etc/nginx/conf.d/nginx.conf --name nginx_spme nginx'
+echo 'docker run -d -p 20201:80 -v $WORKSPACE/dist:/usr/share/nginx/html -v $WORKSPACE/nginx.conf:/etc/nginx/nginx.conf --name nginx_spme nginx'
 docker run -d -p 20201:80 \
 -v $WORKSPACE/dist:/usr/share/nginx/html \
--v $WORKSPACE/nginx.conf:/etc/nginx/conf.d/nginx.conf \
+-v $WORKSPACE/nginx.conf:/etc/nginx/nginx.conf \
 --name nginx_spme nginx
