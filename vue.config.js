3851488d77367d215892c4bdd41d82c57e69bf6a
GetALife <1340482017@qq.com>
2020-03-23 00:00:46 +0800

    update dev config

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://10.60.43.40:20202",
+        target: "http://localhost:20202",
         changeOrigin: true,
         pathRewrite: { "^/api": "" }
       }
