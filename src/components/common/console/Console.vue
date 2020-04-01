1305d6bbe2b7efe1aa282a5abbb6ce3739e5b299
GetALife <1340482017@qq.com>
2020-04-01 20:41:54 +0800

    remove warning of async-validator

--- src/components/common/console/Console.vue
+++ src/components/common/console/Console.vue
@@ -8,6 +8,10 @@
 <script>
 import CMD from "./CMD";
 import JCL from "./JCL";
+// remove warning of async-validator used by a-form
+// ref: https://github.com/yiminghe/async-validator#how-to-avoid-warning
+import Schema from "async-validator";
+Schema.warning = function() {};
 
 export default {
   components: {
