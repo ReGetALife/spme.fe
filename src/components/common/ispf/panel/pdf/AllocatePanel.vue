2912d8b7b50435f296b90b9cfc282d8ca645a4a4
GetALife <1340482017@qq.com>
2020-04-24 00:25:53 +0800

    fix allocate dataset panel

--- src/components/common/ispf/panel/pdf/AllocatePanel.vue
+++ src/components/common/ispf/panel/pdf/AllocatePanel.vue
@@ -12,13 +12,54 @@
       >
       <a-form-item
         class="panel-option"
+        label="Management class: "
+        :label-col="{ span: 8 }"
+        :wrapper-col="{ span: 16 }"
+      >
+        <a-input
+          placeholder="Blank for default management class"
+          v-model="mgntclass"
+        />
+      </a-form-item>
+      <a-form-item
+        class="panel-option"
+        label="Storage class: "
+        :label-col="{ span: 8 }"
+        :wrapper-col="{ span: 16 }"
+      >
+        <a-input
+          placeholder="Blank for default storage class"
+          v-model="storeclass"
+        />
+      </a-form-item>
+      <a-form-item
+        class="panel-option"
         label="Volume Serial: "
         :label-col="{ span: 8 }"
         :wrapper-col="{ span: 16 }"
       >
         <a-input
           placeholder="Blank for system default volume"
-          v-model="volumeName"
+          v-model="volser"
+        />
+      </a-form-item>
+      <a-form-item
+        class="panel-option"
+        label="Device type: "
+        :label-col="{ span: 8 }"
+        :wrapper-col="{ span: 16 }"
+      >
+        <a-input placeholder="Generic unit or device address" v-model="unit" />
+      </a-form-item>
+      <a-form-item
+        class="panel-option"
+        label="Data class: "
+        :label-col="{ span: 8 }"
+        :wrapper-col="{ span: 16 }"
+      >
+        <a-input
+          placeholder="Blank for default data class"
+          v-model="dataclass"
         />
       </a-form-item>
       <a-form-item
@@ -29,7 +70,7 @@
       >
         <a-input
           placeholder="BLKS, TRKS, CYLS, KB, MB, BYTES or RECORDS"
-          v-model="spaceUnits"
+          v-model="alcunit"
         />
       </a-form-item>
       <a-form-item
@@ -38,7 +79,7 @@
         :label-col="{ span: 8 }"
         :wrapper-col="{ span: 16 }"
       >
-        <a-input placeholder="In above units" v-model="priQuantity" />
+        <a-input placeholder="In above units" v-model="primary" />
       </a-form-item>
 
       <a-form-item
@@ -47,7 +88,7 @@
         :label-col="{ span: 8 }"
         :wrapper-col="{ span: 16 }"
       >
-        <a-input placeholder="In above units" v-model="secQuantity" />
+        <a-input placeholder="In above units" v-model="secondary" />
       </a-form-item>
       <a-form-item
         class="panel-option"
@@ -55,10 +96,7 @@
         :label-col="{ span: 8 }"
         :wrapper-col="{ span: 16 }"
       >
-        <a-input
-          placeholder="Zero for sequential data set"
-          v-model="dirBlocks"
-        />
+        <a-input placeholder="Zero for sequential data set" v-model="dirblk" />
       </a-form-item>
       <a-form-item
         class="panel-option"
@@ -66,7 +104,7 @@
         :label-col="{ span: 8 }"
         :wrapper-col="{ span: 16 }"
       >
-        <a-input v-model="recordFormat" />
+        <a-input v-model="recfm" />
       </a-form-item>
       <a-form-item
         class="panel-option"
@@ -74,7 +112,7 @@
         :label-col="{ span: 8 }"
         :wrapper-col="{ span: 16 }"
       >
-        <a-input v-model="recordLen" />
+        <a-input v-model="lrecl" />
       </a-form-item>
       <a-form-item
         class="panel-option"
@@ -82,16 +120,18 @@
         :label-col="{ span: 8 }"
         :wrapper-col="{ span: 16 }"
       >
-        <a-input v-model="blockSize" />
+        <a-input v-model="blksize" />
       </a-form-item>
-
       <a-form-item
         class="panel-option"
         label="Data set name type: "
         :label-col="{ span: 8 }"
         :wrapper-col="{ span: 16 }"
       >
-        <a-input v-model="dsorg" />
+        <a-input
+          placeholder="LIBRARY, HFS, PDS, LARGE, BASIC, EXTREQ, EXTPREF or blank"
+          disabled
+        />
       </a-form-item>
 
       <a-input
@@ -111,49 +151,48 @@ export default {
   name: "allocate-panel",
   data() {
     return {
-      volumeName: "",
-      spaceUnits: "",
-      priQuantity: "",
-      secQuantity: "",
-      dirBlocks: "",
-      recordFormat: "",
-      recordLen: "",
-      blockSize: "",
-      dsorg: ""
+      volser: "",
+      unit: "",
+      alcunit: "",
+      primary: "",
+      secondary: "",
+      dirblk: "",
+      recfm: "",
+      blksize: "",
+      lrecl: "",
+      storeclass: "",
+      mgntclass: "",
+      dataclass: ""
     };
   },
   methods: {
     allocate() {
-      if (this.dsorg.toUpperCase() !== "PDS")
-        this.$message.warn("Please allocate a PDS");
-      else {
-        axios
-          .post("/api/sms/createds", {
-            dsname: this.$store.state.ispf.dataSetName.toUpperCase(),
-            volser: this.volumeName.toUpperCase(),
-            unit: "3390",
-            dsorg: "PO",
-            alcunit: this.spaceUnits.toUpperCase(),
-            dirblk: parseInt(this.dirBlocks),
-            avgblk: 500,
-            primary: parseInt(this.priQuantity),
-            secondary: parseInt(this.secQuantity),
-            recfm: this.recordFormat.toUpperCase(),
-            blksize: parseInt(this.blockSize),
-            lrecl: parseInt(this.recordLen)
-          })
-          .then(res => {
-            if (res.data === "") {
-              this.$message.success("Data set allocated");
-              this.$store.commit("ispf/GO_BACK");
-            } else {
-              this.$message.error("Data set not allocated");
-            }
-          })
-          .catch(e => {
-            this.$message.error("请求错误：" + e.message);
-          });
-      }
+      let params = {
+        dsname: this.$store.state.ispf.dataSetName,
+        volser: this.volser,
+        unit: this.unit,
+        alcunit: this.alcunit,
+        primary: parseInt(this.primary),
+        secondary: parseInt(this.secondary),
+        dirblk: parseInt(this.dirblk),
+        recfm: this.recfm,
+        blksize: parseInt(this.blksize),
+        lrecl: parseInt(this.lrecl),
+        storeclass: this.storeclass,
+        mgntclass: this.mgntclass,
+        dataclass: this.dataclass
+      };
+      // filter empty properties
+      Object.keys(params).forEach(k => !params[k] && delete params[k]);
+      axios
+        .post("/api/dataset", params)
+        .then(() => {
+          this.$message.success("Data set allocated");
+          this.$store.commit("ispf/GO_BACK");
+        })
+        .catch(e => {
+          this.$message.error("请求错误：" + e.message);
+        });
     }
   }
 };
