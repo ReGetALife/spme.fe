e080221f4473cbc74390979fe2229a9a1c606a8f
GetALife <1340482017@qq.com>
2020-04-25 17:29:43 +0800

    refine member list page

--- src/components/common/ispf/panel/pdf/PdslistPanel.vue
+++ src/components/common/ispf/panel/pdf/PdslistPanel.vue
@@ -1,120 +1,174 @@
 <template>
   <div class="pdalist-panel">
-    <h3 class="panel-title">EDIT {{ this.$store.state.ispf.dataSetName }}</h3>
+    <h3 class="panel-title">
+      {{ this.$store.state.ispf.editMode.toUpperCase() }}
+      {{ this.$store.state.ispf.dataSetName }}
+    </h3>
 
-    <a-table :columns="columns" :dataSource="datasets">
-      <a slot="cmd" slot-scope="record">
-        <a-popover title="Enter you command" trigger="click">
-          <a slot="content">
-            <a-input @keyup.enter="onEnter(record.key)" v-model="cmd" />
-          </a>
-          <a-button type="primary" width="10"><a-icon type="edit"/></a-button>
-        </a-popover>
-      </a>
-    </a-table>
+    <a-spin size="large" :spinning="isLoading">
+      <a-icon slot="indicator" type="loading-3-quarters" :spin="true" />
+
+      <a-table :columns="columns" :dataSource="members" rowKey="name">
+        <template slot="cmdTitle">
+          Option
+          <a-popover>
+            <template slot="content">
+              Support 'V', 'E' and 'D' in edit mode. <br />
+              Support 'V' and 'E' in view mode.
+            </template>
+            <a-icon type="question-circle" style="cursor: pointer" />
+          </a-popover>
+        </template>
+        <template slot="cmd" slot-scope="{ name }">
+          <a-input @keyup.enter="onEnter(name, $event)" />
+        </template>
+      </a-table>
+    </a-spin>
 
     <a-input
       addonBefore="Command >"
       @keyup.enter="commandLine"
+      placeholder="Support 'S [Member Name]' in edit mode"
       v-model="cmdLine"
     />
 
-    <hr />
+    <a-modal
+      :title="`确定删除 ${selectedMemberName} 吗？`"
+      v-model="visible"
+      @ok="deleteMember(selectedMemberName)"
+      :confirmLoading="isDeleting"
+    >
+      注意：删除数据集后将不能恢复！
+    </a-modal>
   </div>
 </template>
 
 <script>
+import Axios from "axios";
+
 export default {
   name: "pdslist-panel",
   data() {
     return {
       columns: [
         {
-          title: "Name",
-          dataIndex: "member",
-          width: 400
+          slots: { title: "cmdTitle" },
+          key: "cmd",
+          width: 150,
+          scopedSlots: { customRender: "cmd" }
         },
         {
-          title: "Cmd",
-          key: "cmd",
-          scopedSlots: { customRender: "cmd" }
+          title: "Member Name",
+          dataIndex: "name"
         }
       ],
-      datasets: [],
-      cmd: "",
-      cmdLine: ""
+      members: [],
+      cmdLine: "",
+      isLoading: true,
+      visible: false,
+      selectedMemberName: "",
+      isDeleting: false
     };
   },
-  created() {
+  activated() {
     this.getPdsMemList();
   },
   methods: {
     getPdsMemList() {
-      this.$http
-        .get(
-          `/api/sms/getpdsmemberlist?dsName=${
-            this.$store.state.ispf.dataSetName
-          }`
-        )
+      this.isLoading = true;
+      Axios.get(`/api/dataset/${this.$store.state.ispf.dataSetName}/member`)
         .then(res => {
-          this.datasets = res.data.items;
-          for (let i = 0; i < this.datasets.length; i++)
-            this.datasets[i].key = i;
+          this.members = res.data.map(n => {
+            return { name: n };
+          });
         })
         .catch(err => {
           this.$message.error("请求错误：" + err.message);
+        })
+        .finally(() => {
+          this.isLoading = false;
         });
     },
 
-    onEnter(key) {
-      if (this.cmd.toUpperCase() === "D") {
-        // 删除数据集成员
-        this.$http
-          .delete(
-            "/api/sms/deletepdsmember?dsName=" +
-              this.$store.state.ispf.dataSetName +
-              "(" +
-              this.datasets[key].member +
-              ")"
-          )
+    deleteMember(memberName) {
+      if (memberName) {
+        this.isDeleting = true;
+        Axios.delete(
+          `/api/dataset/${this.$store.state.ispf.dataSetName}(${memberName})`
+        )
           .then(() => {
-            this.$message.success(
-              "Member " + this.datasets[key].member + " deleted"
-            );
-            this.getPdsMemList();
+            this.$message.success("成员 " + memberName + " 删除成功");
           })
-          .catch(err => {
-            this.$message.error("请求错误：" + err.message);
+          .catch(e => {
+            this.$message.error("发生错误：" + e.message);
+          })
+          .finally(() => {
+            this.isDeleting = false;
+            this.visible = false;
+            this.getPdsMemList();
           });
-      } else if (this.cmd.toUpperCase() === "E") {
-        this.$store.commit(
-          "ispf/SET_DS_MEMBER",
-          this.$store.state.ispf.dataSetName +
-            "(" +
-            this.datasets[key].member +
-            ")"
-        );
-        this.$store.commit("ispf/SET_PANEL", "edit");
+      }
+    },
+
+    onEnter(name, event) {
+      let cmd = event.target.value;
+      if (!cmd) {
+        return;
+      }
+      cmd = cmd.toUpperCase();
+
+      switch (cmd) {
+        case "V":
+          this.$store.commit(
+            "ispf/SET_DS_MEMBER",
+            `${this.$store.state.ispf.dataSetName}(${name})`
+          );
+          this.$store.commit("ispf/SET_EDIT_MODE", "view");
+          this.$store.commit("ispf/SET_PANEL", "edit");
+          break;
+        case "E":
+          this.$store.commit(
+            "ispf/SET_DS_MEMBER",
+            `${this.$store.state.ispf.dataSetName}(${name})`
+          );
+          this.$store.commit("ispf/SET_EDIT_MODE", "edit");
+          this.$store.commit("ispf/SET_PANEL", "edit");
+          break;
+        case "D":
+          if (this.$store.state.ispf.editMode === "view") {
+            this.$message.warn("View 模式下不能删除成员");
+          } else {
+            this.selectedMemberName = name;
+            this.visible = true;
+          }
+          break;
+        default:
+          this.$message.warn("暂不支持该操作");
       }
     },
 
     commandLine() {
-      if (this.cmdLine && this.cmdLine[0].toUpperCase() === "S") {
-        let arr = this.cmdLine
-          .trim()
-          .replace(/ +/g, " ")
-          .split(" ");
+      const cmd = this.cmdLine && this.cmdLine.trim().toUpperCase();
+      if (cmd && cmd[0] === "S") {
+        if (this.$store.state.ispf.editMode === "view") {
+          this.$message.warn("View 模式下不能创建成员");
+          return;
+        }
+        let arr = cmd.replace(/ +/g, " ").split(" ");
         if (arr.length < 2) {
           this.$message.warn("请指定 Member Name");
           return;
         }
-        const memName = arr[1].toUpperCase();
+        const memName = arr[1];
         // 创建分区数据集成员
         this.$store.commit(
           "ispf/SET_DS_MEMBER",
           this.$store.state.ispf.dataSetName + "(" + memName + ")"
         );
+        this.$store.commit("ispf/SET_EDIT_MODE", "edit");
         this.$store.commit("ispf/SET_PANEL", "edit");
+      } else {
+        this.$message.warn("暂不支持该操作");
       }
     }
   }
