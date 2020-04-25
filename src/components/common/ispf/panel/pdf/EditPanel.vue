445bfd8aae0e19bee81fa32e2453b8680a7bd5f8
GetALife <1340482017@qq.com>
2020-04-25 19:37:59 +0800

    refine edit page

--- src/components/common/ispf/panel/pdf/EditPanel.vue
+++ src/components/common/ispf/panel/pdf/EditPanel.vue
@@ -1,110 +1,131 @@
 <template>
-  <div class="master-panel" @keyup.114="goBackSave">
-    <h3 class="panel-title">EDIT {{ this.$store.state.ispf.dsMember }}</h3>
-    <a-textarea class="panel-text" :rows="15" v-model="jclText" />
+  <div class="master-panel">
+    <h3 class="panel-title">
+      {{ this.$store.state.ispf.editMode.toUpperCase() }}
+      {{ this.$store.state.ispf.dsMember }}
+    </h3>
+    <a-spin size="large" :spinning="isLoading">
+      <a-icon slot="indicator" type="loading-3-quarters" :spin="true" />
+
+      <a-textarea class="panel-text" :rows="15" v-model="text" />
+    </a-spin>
+
     <br />
     <br />
 
-    <a-row>
-      <a-col :span="21">
-        <div v-focus class="input">
-          <a-input
-            class="panel-option"
-            addonBefore="Command >"
-            @keyup.enter="Command"
-            v-model="cmd"
-          />
-        </div>
-      </a-col>
-      <a-col :span="3">
-        <a-button class="save-button" type="primary" @click="Save"
-          >Save</a-button
-        >
-      </a-col>
-    </a-row>
-    <hr />
+    <a-input
+      class="panel-option"
+      addonBefore="Command >"
+      placeholder="Support 'SAVE' in edit mode and 'SUBMIT(SUB)' in both mode"
+      @keyup.enter="onEnter"
+      v-model="cmd"
+    />
   </div>
 </template>
 
 <script>
+import Axios from "axios";
 export default {
   name: "edit-panel",
   data() {
     return {
       cmd: "",
-      jclText: ""
+      text: "",
+      isLoading: true
     };
   },
-  created() {
-    this.getDsContent();
+  activated() {
+    this.getContent();
   },
   methods: {
-    getDsContent() {
-      // 获取数据集（成员）内容
-      this.$http
-        .get(`/api/sms/getds?dsName=${this.$store.state.ispf.dsMember}`)
+    getContent() {
+      this.text = "";
+      this.isLoading = true;
+      Axios.get(`/api/dataset/${this.$store.state.ispf.dsMember}`)
         .then(res => {
-          this.jclText = res.data;
-          this.jclText.replace(/\s/g, "\t");
+          this.text = res.data;
         })
         .catch(err => {
           this.$message.error("请求错误：" + err.message);
+        })
+        .finally(() => {
+          this.isLoading = false;
         });
     },
-    Write() {
-      // 写入 JCL
-      this.$http
-        .put("/api/sms/writeds", {
-          dsName: this.$store.state.ispf.dsMember,
-          jclList: this.jclText.split("\n")
-        })
+    writeContent() {
+      this.isLoading = true;
+      // axios will use "application/x-www-form-urlencoded" as default content type if data is a string
+      // so it needs to be explicitly set
+      Axios.put("/api/dataset/" + this.$store.state.ispf.dsMember, this.text, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
         .then(() => {
-          this.$message.success("Saved");
+          this.$message.success("保存成功");
         })
         .catch(err => {
           this.$message.error("请求错误：" + err.message);
+        })
+        .finally(() => {
+          this.isLoading = false;
         });
     },
-    Submit() {
-      // 提交 JCL
-      this.$http
-        .put("/api/sms/subjob", {
-          jclList: this.jclText.split("\n")
-        })
+    submitJob() {
+      this.isLoading = true;
+      Axios.post("/api/jcl", {
+        jcl: this.text
+      })
         .then(res => {
-          this.$message.info("Job " + res.data.jobid + " Submitted");
-          this.jclResult(res.data);
+          this.showJobResult(res.data);
         })
         .catch(err => {
           this.$message.error("请求错误：" + err.message);
+        })
+        .finally(() => {
+          this.isLoading = false;
         });
     },
-    Command() {
-      if (this.cmd.toUpperCase() === "SUBMIT") {
-        this.Write();
-        this.Submit();
-      } else
-        this.$message.error("Command " + this.cmd.toUpperCase() + " Not Found");
+    onEnter() {
+      let cmd = this.cmd.trim().toUpperCase();
+      switch (cmd) {
+        case "SUB":
+          this.submitJob();
+          break;
+        case "SUBMIT":
+          this.submitJob();
+          break;
+        case "SAVE":
+          if (this.$store.state.ispf.editMode === "view") {
+            this.$message.warn("View 模式下不能保存更改");
+          } else {
+            this.writeContent();
+          }
+          break;
+        default:
+          this.$message.warn("暂不支持该操作");
+      }
     },
-    goBackSave() {
-      this.Write();
-    },
-    Save() {
-      this.Write();
-    },
-    jclResult(result) {
-      const h = this.$createElement;
-      this.$info({
-        title: "Job Result",
-        content: h("div", {}, [h("p", result)]),
-        onOk() {}
-      });
+    showJobResult(result) {
+      if (result.length > 0) {
+        const h = this.$createElement;
+        this.$info({
+          title: `${result[0].jobId} ${result[0].jobName} 执行结果`,
+          width: 800,
+          content: h("div", {}, [
+            ...result.map(e => {
+              return h("pre", e.output);
+            })
+          ]),
+          onOk() {}
+        });
+      }
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
 .panel-text {
   /* 等宽字体！非常重要！让对齐成为可能 */
   font-family: "Courier New", Courier, monospace;
@@ -112,12 +133,5 @@ export default {
   overflow-x: auto;
   background-color: black;
   color: greenyellow;
-}
-
-.save-button {
-  /* margin-top: 10px; */
-  margin-left: 2px;
-  width: 60px;
-  background-color: black;
 }
 </style>
