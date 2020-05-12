821687740cb7ff4310ca18eae3a21a78e74da11e
GetALife <1340482017@qq.com>
2020-05-12 22:21:24 +0800

    add ACS validate panels

--- src/components/common/ispf/ISPF.vue
+++ src/components/common/ispf/ISPF.vue
@@ -23,12 +23,13 @@ import is_6 from "@/components/common/ispf/panel/ismf/StorageGroup";
 import is_6_5 from "@/components/common/ispf/panel/ismf/StorageGroupVolume";
 import is_6_3_pool from "@/components/common/ispf/panel/ismf/DefinePoolStorageGroup";
 import is_7 from "@/components/common/ispf/panel/ismf/Acs";
+import is_7_2 from "@/components/common/ispf/panel/ismf/TranslateAcs";
+import is_7_3 from "@/components/common/ispf/panel/ismf/ValidateAcs";
 import is_8 from "@/components/common/ispf/panel/ismf/ControlDataset";
 import is_8_1 from "@/components/common/ispf/panel/ismf/DisplayBaseConfig";
 import is_8_2 from "@/components/common/ispf/panel/ismf/DefineBaseConfig";
 import is_8_3 from "@/components/common/ispf/panel/ismf/AlterBaseConfig";
 import is_0_0 from "@/components/common/ispf/panel/ismf/Is00profile";
-import is_7_2 from "@/components/common/ispf/panel/ismf/TranslateAcs";
 import p from "@/components/common/ispf/panel/PdfPanel";
 import p_3_2 from "@/components/common/ispf/panel/pdf/DsuPanel";
 import p_3_2_a from "@/components/common/ispf/panel/pdf/AllocatePanel";
@@ -54,6 +55,7 @@ const componentsMapping = {
   is_6_3_pool,
   is_7,
   is_7_2,
+  is_7_3,
   is_8,
   is_8_1,
   is_8_2,
---
+++ src/components/common/ispf/panel/ismf/ValidateAcs.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="master-panel">
+    <a-spin size="large" :spinning="isLoading">
+      <a-icon slot="indicator" type="loading-3-quarters" :spin="true" />
+      <h3 class="panel-title">TRANSLATE ACS ROUTINES</h3>
+      <div class="panel-content">
+        <br />
+        <a-row class="panel-name">To Perform ACS Translation, Specify:</a-row>
+        <br />
+        <a-row>
+          <a-col>
+            <a-input
+              class="panel-option"
+              addonBefore="SCDS Name"
+              placeholder="1 to 44 Character Data Set Name"
+              v-model="scds"
+            ></a-input>
+          </a-col>
+          <br />
+          <a-col>
+            <a-input
+              class="panel-option"
+              addonBefore="ACS Routine Type"
+              placeholder="DC, MC, SC, SG or *"
+              v-model="type"
+            ></a-input>
+          </a-col>
+          <br />
+          <a-col>
+            <a-input
+              class="panel-option"
+              addonBefore="Listing Data Set"
+              placeholder="1 to 44 Character Data Set Name"
+              v-model="listname"
+            ></a-input>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-input
+            class="panel-option"
+            addonBefore="Command >"
+            placeholder="Use ENTER to Perform ACS Translation"
+            @keyup.enter="onEnter"
+          />
+        </a-row>
+      </div>
+    </a-spin>
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+export default {
+  name: "ValidateAcs",
+  activated() {
+    this.scds = this.$store.state.ispf.cdsName;
+  },
+  data() {
+    return {
+      scds: this.$store.state.ispf.cdsName,
+      type: "",
+      listname: "",
+      isLoading: false
+    };
+  },
+  methods: {
+    onEnter() {
+      if (
+        this.scds.trim().length === 0 ||
+        this.type.trim().length === 0 ||
+        this.listname.trim().length === 0
+      )
+        this.$message.warn("Enter required field");
+      else {
+        this.isLoading = true;
+        const params = {
+          scds: this.scds.trim(),
+          type: this.type.trim().toUpperCase(),
+          listname: this.listname.trim().toUpperCase()
+        };
+        Axios.post("/api/sms/acs/validate", params)
+          .then(res => {
+            this.showResult(res.data);
+          })
+          .catch(e => {
+            this.$message.error("发生错误：" + e.message);
+          })
+          .finally(() => {
+            this.isLoading = false;
+          });
+      }
+    },
+    showResult(result) {
+      if (result.length > 0) {
+        const h = this.$createElement;
+        this.$info({
+          title: `VALIDATE 结果`,
+          width: 800,
+          content: h("div", {}, [h("pre", result)]),
+          onOk() {
+            // do nothing
+          }
+        });
+      }
+    }
+  }
+};
+</script>
+
+<style scoped></style>
